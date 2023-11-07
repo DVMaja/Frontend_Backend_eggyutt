3caebc1d4a3d8c02b595346f9b159caf696b9f43
DVMaja <majadreilinger@gmail.com>
2023-11-07 11:05:08 +0100

    jobb működő verzió

--- Backend/backend/config/cors.php
+++ Backend/backend/config/cors.php
@@ -1,8 +1,8 @@
 <?php
 /** */
-header("Access-Control-Allow-Origin: http://127.0.0.1:5500"); // Módosítsd az eredetet szükség esetén
+/* header("Access-Control-Allow-Origin: http://127.0.0.1:5500"); // Módosítsd az eredetet szükség esetén
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); */
 
 return [
 
@@ -19,7 +19,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
